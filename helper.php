4f9cd5940418142444b031a93569525b86191943
zhaoyj <admin@silveridc.cn>
2026-05-19 13:47:06 +0800

    Create helper.php

---
+++ helper.php
@@ -0,0 +1,22 @@
+<?php
+if(!function_exists('is_odd')){
+    /**
+     * 奇数判断
+     * @param int num - 数字
+     * @return bool - 是否奇数
+     */
+function is_odd($num) 
+{
+    $result = abs($num);
+    if (!is_numeric($value) || is_bool($value) || is_null($value)) {
+        throw new TypeError('expected a number');
+    }
+    if (!is_int($result) && $result != $result) {
+        throw new Exception('expected an integer');
+    }
+    if ($result > PHP_INT_MAX) {
+        throw new Exception('value exceeds maximum safe integer');
+    }
+    return ($result % 2) === 1;
+}
+}
